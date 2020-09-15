632d44b2fbf61fd07c5a82abe928a3d3ee5fb46f
crazylearner <sowmiyachellamuthu@gmail.com>
2020-09-15 21:03:17 +0530

    endpoint renamed sensibly

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var indexRouter = require('./routes/index');
-var testAPIRouter = require("./routes/oidcverify");
+var oidcTokenAPIRouter = require("./routes/oidcverify");
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use('/', indexRouter);
-app.use("/testAPI", testAPIRouter);
+app.use("/oidctoken", oidcTokenAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
